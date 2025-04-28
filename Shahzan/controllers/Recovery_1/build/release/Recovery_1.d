a866fc41f1ff3b3fc9b75f687708265625fda0a5
Shahzank01 <shahzanhafeez2@gmail.com>
2025-04-28 22:24:06 +0500

    Add files via upload

---
+++ Shahzan/controllers/Recovery_1/build/release/Recovery_1.d
@@ -0,0 +1,8 @@
+build/release/Recovery_1.o: Recovery_1.c \
+ C:\Program\ Files\Webots/include/controller/c/webots/camera.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/camera_recognition_object.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/types.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/camera_recognition_object.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/motor.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/robot.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/nodes.h
